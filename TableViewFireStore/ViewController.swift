7afe5f7b952d4cc876b650955ecaeb21010a978f
yasinoki39 <yasinoki39@gmail.com>
2022-04-20 23:28:25 +0900

    Initial Commit

---
+++ TableViewFireStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewFireStore
+//
+//  Created by SHINGO YANAGIDA on 2022/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
